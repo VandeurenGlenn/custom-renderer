41163be2fabf466ee3c3d99c560836a62b29a6c1
Glenn Vandeuren <vandeurenglenn@gmail.com>
2018-08-17 18:31:11 +0200

    Render query based

--- src/render.js
+++ src/render.js
@@ -1,28 +1,16 @@
-const charIt = (chars, string) => `${chars[0]}${string}${chars[1]}`;
-
-// let offset = 0;
-
 /**
  * @param {object} element HTMLElement
  * @param {function} tagResult custom-renderer-mixin {changes: [], template: ''}
  */
-export default (element, tagResult) => {
-  let offset = 0;
+export default (element, {changes, template}) => {
+  if (!changes && !template) return console.warn('changes or template expected');
   if (element.shadowRoot) element = element.shadowRoot;
-  if (!element.innerHTML) {
-    element.innerHTML = tagResult.template
+  if (!element.innerHTML) element.innerHTML = template;
+  for (const key of Object.keys(changes)) {
+    const els = Array.from(element.querySelectorAll(`[render-mixin-id="${key}"]`))
+    for (const el of els) {
+      el.innerHTML = changes[key]
+    }
   }
-  const length = element.innerHTML.length;
-  tagResult.changes.forEach(change => {
-    const position = change.from.position;
-    const chars = [
-      element.innerHTML.charAt(((position[0] - 1) + offset)),
-      element.innerHTML.charAt(((position[1]) + offset))
-    ];
-    element.innerHTML = element.innerHTML.replace(
-      charIt(chars, change.from.value), charIt(chars, change.to.value)
-    );
-    offset = element.innerHTML.length - length
-  })
   return;
 }
