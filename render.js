1479d24a807df29a61118ae471cf7b1e83604b52
Glenn Vandeuren <vandeurenglenn@gmail.com>
2018-08-17 18:32:54 +0200

    :trollface: 1.1.0

--- render.js
+++ render.js
@@ -1,34 +1,20 @@
 var render = (function () {
   'use strict';
 
-  const charIt = (chars, string) => `${chars[0]}${string}${chars[1]}`;
-
-  // let offset = 0;
-
   /**
    * @param {object} element HTMLElement
-   * @param {function} template custom-html templateResult
-   * @param {object} properties {}
+   * @param {function} tagResult custom-renderer-mixin {changes: [], template: ''}
    */
-  var render = (element, template, properties = {}) => {
-    let offset = 0;
-    const result = template(properties);
+  var render = (element, {changes, template}) => {
+    if (!changes && !template) return console.warn('changes or template expected');
     if (element.shadowRoot) element = element.shadowRoot;
-    if (!element.innerHTML) {
-      element.innerHTML = result.template;
+    if (!element.innerHTML) element.innerHTML = template;
+    for (const key of Object.keys(changes)) {
+      const els = Array.from(element.querySelectorAll(`[render-mixin-id="${key}"]`));
+      for (const el of els) {
+        el.innerHTML = changes[key];
+      }
     }
-    const length = element.innerHTML.length;
-    result.changes.forEach(change => {
-      const position = change.from.position;
-      const chars = [
-        element.innerHTML.charAt(((position[0] - 1) + offset)),
-        element.innerHTML.charAt(((position[1]) + offset))
-      ];
-      element.innerHTML = element.innerHTML.replace(
-        charIt(chars, change.from.value), charIt(chars, change.to.value)
-      );
-      offset = element.innerHTML.length - length;
-    });
     return;
   }
 
